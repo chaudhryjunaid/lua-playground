25a521394654f6e9c97feeba9a5b83422075a00e
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2020-09-26 16:16:11 +0500

    improvements to romantodec

--- romantodecimal.lua
+++ romantodecimal.lua
@@ -2,15 +2,14 @@ function romantodecimal(roman)
   local romanchars = {i=1, v=5, x=10, l=50, c=100, d=500, m=1000}
   local romantable = {}
   local i = 1
-  for v in string.gmatch(roman, '.') do
+  for v in string.gmatch(string.lower(roman), '.') do
     romantable[i] = v
     i = i + 1
   end
   local decimal = 0;
-  local currentdecimal = 0;
   local tablelength = #romantable
   for j = 1,tablelength do
-    currentdecimal = romanchars[romantable[j]]
+    local currentdecimal = romanchars[romantable[j]]
     local nextdecimal = j < tablelength and romanchars[romantable[j+1]] or 0
     if nextdecimal > currentdecimal then
       decimal = decimal - currentdecimal
